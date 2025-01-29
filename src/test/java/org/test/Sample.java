010c4f9c803b28b32764ddb5ab8b9ab8f77a592e
uma30012001 <kuttalamuma@gmail.com>
2025-01-29 13:52:25 +0530

    Arun 2nd code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("say hi");
 		System.out.println("Kumar write a code");
+		System.out.println("Arun 2nd code");
 
 	}
 
